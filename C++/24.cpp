a79dd3b54375c291a4cb24bd8af6c73915a15d8c
NAZIR HUSSAIN <khnitaly00@gmail.com>
2025-01-04 17:54:04 +0530

    Add files via upload

---
+++ C++/24.cpp
@@ -0,0 +1,15 @@
+//Working with string
+/*Use of string to find the length*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	string str1;
+	cout << "Enter the string: ";
+	cin >> str1;
+	cout << endl<< "Lenth of string: " << str1.length();
+		
+	return 0;
+}
